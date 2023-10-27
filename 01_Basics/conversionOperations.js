b377ef4ad59a85e0e1898c40cb9895b8fd1940b9
devraj62891 <70874547+devraj62891@users.noreply.github.com>
2023-10-27 20:11:15 +0000

    conversion of datatype

---
+++ 01_Basics/conversionOperations.js
@@ -0,0 +1,19 @@
+//very very important 
+let score="33abc";
+console.log(typeof score)
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+console.log(null)
+
+//"33"=33
+//"33abc"=NaN==not a number
+//true=1 false=0
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
