7ec6ebd106a39990ef755803fb64780016c6bfce
devraj62891 <70874547+devraj62891@users.noreply.github.com>
2023-10-27 19:54:32 +0000

    datatypes done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="devraj62891@gmail.com"
+var accountPassword="12345"
+accountcity="kolkata"
+let accountState;
+/*
+Prefer not to use var because of issue in block and functional block
+*/
+// accountId=2 not allowed
+accountEmail="hc@hc.com"
+accountPassword="212123133"
+accountcity="jaipur"
+console.log(accountId);
+console.table([ accountId, accountEmail, accountPassword, accountcity, acccountState])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js oode as newer version
+// alert(3+3)  //we are using node js not browser
+
+console.log(3+3); 
+console.log("devraj")
+let name="devraj"
+let age=18
+let isLoggedIn=false
+
+//number - 2 to power 53
+//bigint
+//string=""
+//boolean==true or false
+//null=standalone value
+//undefined== variable declared but value not assigned
+//symbol==to find uniqueness
+//object--
+
+console.log(typeof "devraj")
+console.log(typeof age)
+console.log(typeof null)
