c8d4a8d264ea028c87b837ed02c05a7113bed650
devraj62891 <70874547+devraj62891@users.noreply.github.com>
2023-10-28 19:41:48 +0000

    conversion done

--- 01_Basics/conversionOperations.js
+++ 01_Basics/conversionOperations.js
@@ -1,19 +1,42 @@
 //very very important 
 let score="33abc";
-console.log(typeof score)
+// console.log(typeof score)
 let valueInNumber=Number(score);
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
-console.log(null)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// console.log(null)
 
 //"33"=33
 //"33abc"=NaN==not a number
 //true=1 false=0
 let isLoggedIn=1
 let booleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//*************************Operations*********** */
+let value=3
+let negVALUE=-value
+// console.log(negVALUE);
+let str1="hello"
+let str2=" devraj"
+let str3=str1+str2;
+// console.log(str3)
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+2)//if first value is string then all other will be treated like string and vice versa
+
+console.log(1+2+"2")
+// console.log(true)
+console.log(+"")
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
 
